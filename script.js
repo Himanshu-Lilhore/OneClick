f8fb592ddb44f51b3bf21160665c15d3d5a7e8e9
himanshu-sec <himanshu.other.email@gmail.com>
2024-02-20 01:17:32 +0530

    Commit_20-Feb-24( 1:17)

--- script.js
+++ script.js
@@ -7,14 +7,34 @@ let desiredOptions = [
     'unknown'
 ]
 
+let colors = [
+    "#2196f3", // blue
+    "#EF6C00", // orange
+    "#4caf50", // green
+    "#ab47bc", // magenta
+    "#FFEB3B", // yellow
+]
+
 var dropdowns = document.querySelectorAll('select');
 var count = 0;
 let holder = document.createElement("DIV");
-holder.style = "padding: 3px; border-width: 1px; position: absolute; top: 0; right: 0; z-index: 9999;"
-
+holder.style = "min-width: 16rem; padding: 3px; border-width: 1px; position: sticky; top: 0; right: 0; z-index: 9999;"
+let auto =  document.createElement("BUTTON")
+auto.style = "padding : 1px; border-width: 3px; display: block; margin: 8px 0px; font-weight: bold;"
+auto.textContent = 'AUTO'
+auto.addEventListener('click', ()=>{auto.style.backgroundColor = "rgb(47 192 47 / 95%)";})
+holder.appendChild(auto)
 
 function selOA(dropdown, val){
     dropdown.value = val;
+    dropdown.style.borderStyle = "dashed"
+}
+
+let currColorIndex = -1
+function getNextColor(){
+    currColorIndex++
+    if(currColorIndex >= colors.length) currColorIndex = 0
+    return colors[currColorIndex]
 }
 
 dropdowns.forEach(function(dropdown, index) {
@@ -28,18 +48,30 @@ dropdowns.forEach(function(dropdown, index) {
     for (var i = 0; i < options.length; i++) {
         if (desiredOptions.includes(options[i].textContent.trim().toLowerCase())) {
             let val = options[i].value;
+            let currColor = getNextColor();
+            dropdown.style.borderWidth = "2px"
 
-            // auto select all :
-                // dropdown.value = val;
+            // // FULL AUTO : 
+            // // auto select all without button :
+            //     selOA(dropdown, val)
+            //     dropdown.style.borderColor = "#4caf50"
+            
+            // MANUAL : 
+            // auto select all with button :
+                auto.addEventListener('click', () => {selOA(dropdown, val)});
 
             // Give buttons for selection :
+                let divtemp = document.createElement("DIV")
                 let btemp = document.createElement("BUTTON");
-                btemp.textContent = "sel-"+index;
-                btemp.style = "padding : 3px; border-width: 1px;"
-                    // btemp.style = "padding: 3px; border-width: 1px; position: absolute; top: 0; right: 0; z-index: 9999;"
+                btemp.textContent = (index+1) + ". " + val;
+                divtemp.style = "padding-left : 15px; border-width: 1px; display: block; margin: 15px 0px; width: fit-content;"
+                btemp.style = "padding: 5px 0px; border-width: 1px;"
                 btemp.addEventListener('click', () => {selOA(dropdown, val)});
-                holder.appendChild(btemp);
-                    // document.body.appendChild(btemp, );
+                btemp.addEventListener('click', () => {divtemp.style.paddingLeft = "2px"});
+                divtemp.style.backgroundColor = currColor
+                dropdown.style.borderColor = currColor
+                divtemp.appendChild(btemp)
+                holder.appendChild(divtemp)
                 
             count++;
             break;
