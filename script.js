6ee4833d78860544a701a6ac2ac3b77e51a85d4c
himanshu-sec <himanshu.other.email@gmail.com>
2024-02-19 21:01:08 +0530

    Commit_19-Feb-24(21:01)

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 // Content script (script.js) to interact with the currently open page
+let desiredOptions = [
+    'other acceptable',
+    'other unearned',
+    'us passport',
+    'in this home',
+    'unknown'
+]
+
 var dropdowns = document.querySelectorAll('select');
 var count = 0;
 
@@ -9,14 +17,21 @@ function selOA(dropdown, val){
 dropdowns.forEach(function(dropdown, index) {
     var options = dropdown.options;
     for (var i = 0; i < options.length; i++) {
-        if (options[i].textContent.trim() === 'Other acceptable') {
-            let btemp = document.createElement("BUTTON");
-            btemp.textContent = "OA"+index;
-            btemp.style = "padding : 3px; border-width: 1px;"
+        if (desiredOptions.includes(options[i].textContent.trim().toLowerCase())) {
+            if(options[i].attributes.type === 'hidden' || options[i].attributes.disabled) continue
             let val = options[i].value;
-            btemp.addEventListener('click', () => {selOA(dropdown, val)});
-            document.body.appendChild(btemp);
-            // dropdown.value = options[i].value;
+
+            // auto select all :
+                // dropdown.value = val;
+
+            // Give buttons for selection :
+                let btemp = document.createElement("BUTTON");
+                btemp.textContent = "OA"+index;
+                    // btemp.style = "padding : 3px; border-width: 1px;"
+                btemp.style = "padding: 3px; border-width: 1px; position: absolute; top: 0; right: 0; z-index: 9999;"
+                btemp.addEventListener('click', () => {selOA(dropdown, val)});
+                document.body.appendChild(btemp, );
+                dropdown.value = options[i].value;
             count++;
             break;
         }
